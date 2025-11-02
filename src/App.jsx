cabab015638fa76e800910b34f8d15c3c7a6426e
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2025-11-02 20:06:04 +0700

    custom lại axios

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import axios from "./util/axios.customize";
+import { useEffect } from "react"
+
+function App() {
+
+  useEffect(()=>{
+    const fetchHelloWorld = async() => {
+      const res = await axios.get(`/v1/api`);
+      console.log(res);
+    }
+
+    fetchHelloWorld()
+  },[]);
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+    </>
+  )
+}
+
+export default App
