0dcfd09188c36271afd48f00566f920668540826
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2025-11-05 23:07:33 +0700

    done getUser

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,9 +1,58 @@
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+import { getUserApi } from '../util/api';
+
 const UserPage = () => {
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(()=> {
+        const fetchUser = async() => {
+            const res = await getUserApi();
+            if(res){
+                setDataSource(res)
+            }
+        }
+        fetchUser();
+    }, []);
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: '_id',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Role',
+            dataIndex: 'role',
+        },
+    ];
+
     return (
-        <div>
-            User page nè
+        <div
+            style={{
+                maxWidth: 1200,
+                margin: "40px auto",
+                background: "#fff",
+                borderRadius: "12px",
+                boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+                padding: "24px",
+            }}
+        >
+            <Table
+                dataSource={dataSource}
+                columns={columns}
+                bordered
+                rowKey={"_id"}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default UserPage;
--- src/util/axios.customize.js
+++ src/util/axios.customize.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL
-
 });
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
+    config.headers.Authorization = `Bearer ${localStorage.getItem("access_token")}`;
     return config;
   }, function (error) {
     // Do something with request error
