f63dbd2beddca66963903eb0c883f675603ceacb
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2025-11-12 09:17:11 +0700

    fix F5

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/header";
 import axios from "./util/axios.customize";
-import { useEffect } from "react"
+import { useContext, useEffect } from "react"
+import { AuthContext } from "./components/context/auth.context";
+import { Spin } from "antd";
 
 function App() {
 
+  const {setAuth, appLoading, setAppLoading} = useContext(AuthContext);
   useEffect(()=>{
-    const fetchHelloWorld = async() => {
-      const res = await axios.get(`/v1/api`);
-      console.log(res);
+    const fetchAccount = async() => {
+      setAppLoading(true);
+
+      const res = await axios.get(`/v1/api/account`);
+      if(res){
+        setAuth({
+          isAuthenticated: true,
+          user: {
+              email: res.email,
+              name: res.name,
+          }
+        })
+      }
+      setAppLoading(false);
     }
 
-    fetchHelloWorld()
+    fetchAccount()
   },[]);
 
   return (
-    <>
-      <Header/>
-      <Outlet/>
-    </>
+    <div>
+      {appLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <Spin size="large" />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      )}
+    </div>
   )
 }
 
